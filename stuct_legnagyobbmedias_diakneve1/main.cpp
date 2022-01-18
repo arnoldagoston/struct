2a4308f1cb4f98fb47d0a080102bebe7d67a4ced
arnoldagoston <91243618+arnoldagoston@users.noreply.github.com>
2022-01-18 09:26:38 +0200

    Add files via upload

---
+++ stuct_legnagyobbmedias_diakneve1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+struct diak{
+int azonosito;
+string nev;
+string osztaly;
+double media;
+} diakok[1000];
+
+int main()
+{
+    int max, idx;
+    int n;
+    ifstream in("input.txt");
+    input>>n;
+    for (int i=0;i<n;i++) {
+        in >> diakok[i].azonosito;
+        in >> diakok[i].nev;
+        in >> diakok[i].osztaly;
+        in >> diakok[i].media;
+
+    if (i==0) {
+        max=diakok[i].media;
+    }
+    if (diakok[i].media>max) {
+        max=diakok[i].media;
+        idx=diakok[i].azonosito;
+    }
+    }
+    cout << idx << endl;
+    return 0;
+}
